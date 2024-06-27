48412f5a4d0d08dfb1ff5593c502b72390bdbf28
Daniel <danielukoha101@gmail.com>
2024-06-27 20:44:47 +0100

    feat: fetch from api routes

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -1,9 +1,39 @@
-import { Text, View } from "react-native";
+import { Currency } from "@/interfaces/crypto";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { Image, Text, View } from "react-native";
 
 const Page = () => {
+  const currencies = useQuery({
+    queryKey: ["listings"],
+    queryFn: () =>
+      fetch("http://172.20.10.2:8081/api/listings").then((res) => res.json()),
+  });
+
+  const ids = currencies.data
+    ?.map((currency: Currency) => currency.id)
+    .join(",");
+
+  const { data } = useQuery({
+    queryKey: ["info", ids],
+    queryFn: () =>
+      fetch(`http://172.20.10.2:8081/api/info?ids=${ids}`).then((res) =>
+        res.json()
+      ),
+    enabled: !!ids,
+  });
+
   return (
     <View>
-      <Text>Page</Text>
+      {currencies.data?.map((currency: Currency) => (
+        <View key={currency.id} style={{ flexDirection: "row" }}>
+          <Image
+            source={{ uri: data?.[currency.id].logo }}
+            style={{ width: 32, height: 32 }}
+          />
+
+          <Text>{currency.name}</Text>
+        </View>
+      ))}
     </View>
   );
 };
